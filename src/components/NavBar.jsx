6c8b1fb9ef336fb76ad4d75a0c1838a66f21a504
orellbrooks <orellbrooks0@gmail.com>
2026-04-28 23:31:50 -0400

    NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react'; // 1. Add useContext
+import { Link } from 'react-router-dom';
+import { AppContext } from '../context/AppContext'; // 2. Import the Context
 
 const NavBar = () => {
+  const { appTitle } = useContext(AppContext); // 3. Grab the title from Context
+
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm">
       <div className="container">
-        <a className="navbar-brand" href="/">Yard Manager</a>
+        {/* Now the Brand Name comes from your Global Context! */}
+        <Link className="navbar-brand fw-bold" to="/">{appTitle}</Link>
         <div className="navbar-nav">
-          <a className="nav-link" href="/">Home</a>
-          <a className="nav-link" href="/yards">Yards</a>
-          <a className="nav-link" href="/about">About</a>
+          <Link className="nav-link" to="/">Home</Link>
+          <Link className="nav-link" to="/yards">View Yards</Link>
+          <Link className="nav-link" to="/about">About</Link>
         </div>
       </div>
     </nav>
